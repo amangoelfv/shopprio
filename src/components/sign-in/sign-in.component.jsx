971461240db5b018f3b84b01af501b0c03793336
amangoelfv <amangoel300@gmail.com>
2021-10-08 13:51:08 +0530

    added signin

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './sign-in.styles.scss';
+import FormInput from './../form-input/form-input.component';
+import CustomButton from './../custom-button/custom-button.component';
+
+const SignIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setEmail('');
+        setPassword('');
+    }
+    return (
+        <div className='sign-in'>
+            <h2>I already have an account</h2>
+            <span>Sign in with your email and password</span>
+
+            <form onSubmit={handleSubmit}>
+                <FormInput
+                    name='email'
+                    type='email'
+                    value={email}
+                    label={"Email"}
+                    required
+                    handleChange={(e) => setEmail(e.target.value)}
+                />
+                <FormInput
+                    name='password'
+                    type='password'
+                    label={"Password"}
+                    value={password}
+                    required
+                    handleChange={(e) => setPassword(e.target.value)}
+                />
+
+                <CustomButton type='submit'>SIGN IN</CustomButton>
+            </form>
+        </div>
+    )
+}
+
+export default SignIn
