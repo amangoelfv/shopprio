1ccbad5ecd9ab5d0149b5459e2405e0d9e133deb
amangoelfv <amangoel300@gmail.com>
2021-11-09 15:11:54 +0530

    updating redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInPage from './pages/signin/signin.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { onSnapshot } from "firebase/firestore";
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
 
-function App() {
-  const [currentUser, setCurrentUser] = useState(null);
+function App({ setCurrentUser, currentUser }) {
   useEffect(() => {
     const authSubscription = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -30,14 +31,19 @@ function App() {
   }, [])
   return (
     <div>
-      <Header currentUser={currentUser} />
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInPage} />
+        <Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : <SignInPage />} />
       </Switch>
     </div>
   );
 }
-
-export default App;
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import './header.style.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from './../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 const Header = ({ currentUser }) => (
     <div className='header'>
         <Link to="/" className='logo-container'>
@@ -13,7 +14,7 @@ const Header = ({ currentUser }) => (
             <Link className='option' to='/contact'>CONTACT</Link>
             {
                 currentUser ?
-                    <div className='option' onClick = {() => auth.signOut()}>SIGN OUT</div> :
+                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> :
                     <Link className='option' to='/signin'>SIGN IN</Link>
             }
         </div>
@@ -21,4 +22,7 @@ const Header = ({ currentUser }) => (
     </div>
 )
 
-export default Header
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+export default connect(mapStateToProps)(Header);
